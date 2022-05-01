9fcec8c49897ea347acc98e8d37e818ce3574202
joechip125 <52743358+joechip125@users.noreply.github.com>
2022-05-01 15:50:39 +0200

    created part1

---
+++ assignments/Part1/TimeServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TimeServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
