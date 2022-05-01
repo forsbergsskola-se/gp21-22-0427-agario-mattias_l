c77698bfbee2a525e659bd3e649cbe0f7c2d2f94
joechip125 <52743358+joechip125@users.noreply.github.com>
2022-05-01 17:23:50 +0200

    update

---
+++ assignments/Part1/TimeServer2/Server/Listener.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    public class Listener
+    {
+        private TcpListener server;
+        Byte[] bytes = new Byte[256];
+        String data = null;
+
+        public void Init()
+        {
+            Int32 port = 13000;
+            IPAddress localAddress = IPAddress.Parse("127.0.0.1");
+
+            server = new TcpListener(localAddress, port);
+            server.Start();
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                
+            }
+        }
+        
+    }
+}
---
+++ assignments/Part1/TimeServer2/Server/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Sockets;
+namespace Server
+{
+    class Program
+    {
+        private TcpClient client = null;
+        private TcpListener server = null;
+
+        private static Listener _listener; 
+        
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
