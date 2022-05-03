660c8d6286dfb5d1408aa250ca89ff62773e7067
joechip125 <52743358+joechip125@users.noreply.github.com>
2022-05-03 16:42:18 +0200

    created script

---
+++ assignments/Agario/Assets/Scripts/RequsetServerTime.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class RequestServerTime : MonoBehaviour
+{
+    private TcpClient _client = new TcpClient();
+
+    private void Awake()
+    {
+        
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
