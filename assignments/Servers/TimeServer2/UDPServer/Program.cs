3b874c8c1d9833f2d369f3868c83cbbc15545ce6
joechip125 <52743358+joechip125@users.noreply.github.com>
2022-05-05 13:51:58 +0200

    started asignment 3

--- assignments/Servers/TimeServer2/UDPServer/Program.cs
+++ assignments/Servers/TimeServer2/UDPServer/Program.cs
@@ -1,19 +1,28 @@
 ﻿using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 
 namespace UDPServer
 {
     class Program
     {
-        private static UdpClient client = new UdpClient(1500);
+        private static UdpClient server = new UdpClient(1500);
         
         private static EndPoint _endPoint;
+      
    
 
         static void Main(string[] args)
         {
-            
+            new Thread(() =>
+            {
+                while (true)
+                {
+                    var remoteEP = new IPEndPoint(IPAddress.Any, 1500);
+                    var data = server.Receive(ref remoteEP); 
+                }
+            }).Start();
         }
     }
 }
